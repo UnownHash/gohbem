47338b09ed83deecdbfb93e68b3ead5a8c89e509
Jakub <10072920+lenisko@users.noreply.github.com>
2023-06-23 14:45:20 +0200

    feat: add log interface to Ohbem conf struct (#15)
    * fix: log to stdout by default
    
    * feat: add log interface to Ohbem conf struct
    
    * feat: rework log

--- structs.go
+++ structs.go
@@ -16,6 +16,25 @@ type Ohbem struct {
 	WatcherInterval       time.Duration
 	compactRankCache      sync.Map
 	watcherChan           chan bool
+	Logger                Logger
+}
+
+// Logger interface
+//
+// Example implementation:
+//
+//	type CustomLogger struct{}
+//	func (cl *CustomLogger) Print(message string) {
+//		fmt.Println("CustomLogger:", message)
+//	}
+//	logger := &CustomLogger{}
+//	ohbem := Ohbem{Logger: logger, ...}
+//
+// Notes:
+//   - The implementation of the Logger interface defines the specific behavior of the logging operation.
+//   - The method should handle the formatting and output of the Log message according to the logger's rules.
+type Logger interface {
+	Print(message string)
 }
 
 // League struct is holding one entry of League configuration passed to Ohbem struct.
